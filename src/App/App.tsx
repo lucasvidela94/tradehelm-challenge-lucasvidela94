03d349bc55754fda84f5985d3755a1dbd9be402c
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2022-05-27 00:42:59 -0300

    Set items done and fetch data from initial status done

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,35 @@
 import * as React from "react";
 
+import api from "../Item/api";
+import Item from "../Item/types";
+
 import styles from "./App.module.scss";
 
+enum Status {
+  Initial = "Initial",
+  Complete = "Complete",
+}
+
 const App: React.FC = () => {
+  const [items, setItems] = React.useState<Item[]>([]);
+  const [status, setStatus] = React.useState<Status>(Status.Initial);
+
+  React.useEffect(() => {
+    api.list().then((items) => {
+      setItems(items);
+      setStatus(Status.Complete);
+    });
+  }, []);
+
+  if (status === Status.Initial) {
+    return <span>Cargando....</span>;
+  }
+
   return (
     <main className={styles.container}>
       <header className={styles.header}>
-        <h1>
-          <img alt="TradeHelm" src="/logo.svg" />
-        </h1>
-        <h3>Lets get this party started</h3>
+        <h1>Lista del super mercado</h1>
+        <h3>{items.length} items(s)</h3>
       </header>
     </main>
   );
