e9e084d565901ea5d7ed749b52f701600dcdfc86
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2022-05-27 01:35:18 -0300

    Create list functionality with proper components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,8 +2,15 @@ import * as React from "react";
 
 import api from "../Item/api";
 import Item from "../Item/types";
+import Button from "../Interface/Manageble/Buttons/Buttons";
+import Form, {FormFooter} from "../Interface/Manageble/Form";
+import TextField from "../Interface/Input/TextField/TextField";
 
 import styles from "./App.module.scss";
+
+interface Form extends HTMLFormElement {
+  text: HTMLInputElement;
+}
 
 enum Status {
   Initial = "Initial",
@@ -13,6 +20,21 @@ enum Status {
 const App: React.FC = () => {
   const [items, setItems] = React.useState<Item[]>([]);
   const [status, setStatus] = React.useState<Status>(Status.Initial);
+  const [isFormVisible, toggleForm] = React.useState<boolean>(false);
+
+  function remove(id: Item["id"]) {
+    api.remove(id).then(() => setItems((items) => items.filter((item) => item.id !== id)));
+  }
+
+  function add(event: React.FormEvent<Form>) {
+    event.preventDefault();
+
+    const text = event.currentTarget.text.value.trim();
+
+    if (!text) return;
+    api.create(text).then((item) => setItems((items) => items.concat(item)));
+    toggleForm(false);
+  }
 
   React.useEffect(() => {
     api.list().then((items) => {
@@ -31,6 +53,31 @@ const App: React.FC = () => {
         <h1>Lista del super mercado</h1>
         <h3>{items.length} items(s)</h3>
       </header>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={() => remove(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <Button colorScheme="primary" onClick={() => toggleForm(true)}>
+        Añadir item
+      </Button>
+      {isFormVisible && (
+        <Form onClose={() => toggleForm(false)}>
+          <form onSubmit={add}>
+            <h2>Añadir item</h2>
+            <TextField name="text" />
+            <FormFooter>
+              <Button type="button">Cancel</Button>
+              <Button colorScheme="primary" type="submit">
+                Añadir
+              </Button>
+            </FormFooter>
+          </form>
+        </Form>
+      )}
     </main>
   );
 };
